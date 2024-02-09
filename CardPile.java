e341f1e3b9763321903b3b6d535abf9458df17f9
UJcodez <94002873+UJcodez@users.noreply.github.com>
2024-02-08 22:09:14 -0500

    Add files via upload

---
+++ CardPile.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coe318.lab5;
+
+
+/**
+ *
+ * @ Usayd Jahangiri
+ */
+
+import java.util.ArrayList;
+
+/**
+ * A pile of cards.
+ *
+ */
+public class CardPile {
+    //Instance variables
+    private ArrayList<Card> cards = new ArrayList<Card>();;
+
+    public CardPile() {
+        //Initialize the instance variable.
+        
+
+    }
+    /**
+     * Add a card to the pile.
+     * @param card
+     */
+    public void add(Card card) {
+        //FIX THIS
+        this.cards.add(card);
+    }
+
+    /**
+     * Remove a card chosen at random from the pile.
+     * @return
+     */
+    public Card removeRandom() {
+        int randNum = (int)(Math.random()*this.cards.size());
+        return(this.cards.remove(randNum));
+        //FIX THIS
+    }
+
+    /**
+     * The string representation is a space separated list
+     * of each card.
+     * @return
+     */
+    @Override
+    public String toString() {
+        String listCards = "\n";
+        for(Card card : this.cards){
+            listCards += card.toString() + "\n";
+        }
+        return listCards;
+    }
+        
+        //FIX THIS
+       
+    
+    /**
+     * @return the cards
+     */
+    public ArrayList<Card> getCards() {
+        return this.cards;
+    }
+
+    public static void main(String[] args) {
+        CardPile p = new CardPile();
+        p.add(new Card(2, 1, true));
+        p.add(new Card(3, 2, true));
+        p.add(new Card(4, 3, false));
+        p.add(new Card(14, 1, true));
+        System.out.println("Removed: " + p.removeRandom());
+        System.out.println("Removed: " + p.removeRandom());
+        System.out.println("Removed: " + p.removeRandom());
+        System.out.println("Removed: " + p.removeRandom());
+        System.out.println("");
+        CardPile deck = new CardPile();
+        for(int i = 2; i < 15; i++) {
+            for(int j = 0; j < 4; j++) {
+                deck.add(new Card(i, j, true));
+            }
+        }
+        for (int i = 0; i < 52; i++) {
+            System.out.println((i+1) + ": " + deck.removeRandom());
+
+        }
+    }
+
+
+}
