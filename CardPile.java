447c287081f89ccafe24921b07d053081085bbb0
UJcodez <94002873+UJcodez@users.noreply.github.com>
2024-02-08 22:11:30 -0500

    Update CardPile.java

--- CardPile.java
+++ CardPile.java
@@ -2,14 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package coe318.lab5;
-
-
-/**
- *
- * @ Usayd Jahangiri
- */
-
 import java.util.ArrayList;
 
 /**
